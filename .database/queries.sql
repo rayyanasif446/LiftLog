fbdec60bc6cee2f52699b7cf43dec8e027c0204b
Rayyan Asif <rayyanasif446@education.nsw.gov.au>
2026-04-25 04:11:09 +1000

    Added test data (gym logs)

--- .database/queries.sql
+++ .database/queries.sql
@@ -1,13 +1,16 @@
-CREATE TABLE IF NOT EXISTS Users (
-    id  INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL
-);
+--CREATE TABLE IF NOT EXISTS Users (
+--    id  INTEGER PRIMARY KEY AUTOINCREMENT,
+--    username TEXT NOT NULL UNIQUE,
+--    password TEXT NOT NULL
+--);
 
-CREATE TABLE IF NOT EXIST Sessions (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER NOT NULL,
-    date TEXT NOT NULL,
-    
-    FOREIGN KEY (user_id) REFERENCES Users(id)
-);
+--CREATE TABLE IF NOT EXISTS Sessions (
+--    id         INTEGER PRIMARY KEY AUTOINCREMENT,
+--    user_id    INTEGER NOT NULL,
+--    date       TEXT    NOT NULL,
+--    exercise   TEXT    NOT NULL,
+--    sets       INTEGER NOT NULL,
+--    reps       INTEGER NOT NULL,
+--    weight_kg  REAL    NOT NULL,
+--    FOREIGN KEY (user_id) REFERENCES Users(id)
+--)
---
+++ setup_db.py
@@ -0,0 +1,62 @@
+#Data base setup and test
+import sqlite3
+from werkzeug.security import generate_password_hash
+
+conn = sqlite3.connect('.database/liftlog.db')
+
+
+conn.execute('''
+    CREATE TABLE IF NOT EXISTS Users (
+        id       INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT    NOT NULL UNIQUE,
+        password TEXT    NOT NULL
+    )
+''')
+
+conn.execute('''
+    CREATE TABLE IF NOT EXISTS SESSIONS (
+        id       INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id  INTEGER NOT NULL,
+        date     TEXT    NOT NULL,
+        exercise TEXT    NOT NULL,
+        sets     INTEGER NOT NULL,
+        reps     INTEGER NOT NULL,
+        weight_kg REAL   NOT NULL,
+        FOREIGN KEY (user_id REFERENCES User(id)
+    )
+''')
+
+#seed test users (randomly genereated)
+('marcus', 'ironmind')
+('priya', 'deadlift99')
+('zach', "squatking")
+('taylah', 'gainz2026')
+('oliver', 'benchpress')
+
+for username, password in users:
+    conn.execute(
+        'INSERT OR IGNORE INTO Users(username, passwor) VALUES(?, ?)',
+        (username, generate_password_hash(password))
+    )
+conn.commit()
+
+#seed test sessions (just to give an example)
+sessions = [
+    (1, '10-04-2026', 'Bench Press',            4, 8, 80.0),
+    (1, '12-04-2026', 'Incline Dumbell Press',  3, 10, 30.0),
+    (2, '11-04-2026', 'Deadlift',               5, 5, 12.0),
+    (2, '15-04-2026', 'Lateral Raise',          4, 8, 90.0),
+    (3, '10-04-2026', 'Back Squat',             4, 12, 180.0),
+    (4, '14-04-2026', 'Leg Press',              4, 10, 80.0),
+    (4, '11-04-2026', 'Hip Thrust',             3, 10, 40.0),
+    (5, '15-04-2026', 'Bulgarian Split squat',  4, 6, 60.0),
+    (5, '12-04-2026', 'Overhead Press',         3, 15, 12.0),
+]
+conn.executemany(
+    'INSERT OR IGNOR INTO Sessions'
+    '(user_id, date, exercise, sets, reps, weight_kg)'
+    ' VALUES(?, ?, ?, ?, ?, ?)'
+    sessions
+)
+conn.commit()
+conn.close()
